3c85c36ad6cb902abdac82c0bd039696757eba9d
lightskintyga <daniilivanov7010@gmail.com>
2023-06-06 05:05:39 +0500

    main

---
+++ js/mobile__menu.js
@@ -0,0 +1,20 @@
+const menu = document.querySelector('.nav');
+const menuBtn = document.querySelector('.menu__icon');
+
+const body = document.body;
+
+if (menu && menuBtn) {
+    menuBtn.addEventListener('click', () => {
+        menu.classList.toggle('active')
+        menuBtn.classList.toggle('active')
+        body.classList.toggle('lock')
+    })
+
+    menu.querySelectorAll('.nav__link').forEach(link => {
+        link.addEventListener('click', () => {
+            menu.classList.remove('active')
+            menuBtn.classList.remove('active')
+            body.classList.remove('lock')
+        })
+    })
+}
